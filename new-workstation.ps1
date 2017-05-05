3a59745ff7cf47b95a3278dffaa528f887fafbdd
John J Kavanagh <john.kavanagh@outlook.com>
2017-05-05 15:42:44 -0400

    formatted for advanced function/script

--- new-workstation.ps1
+++ new-workstation.ps1
@@ -2,8 +2,8 @@
 .SYNOPSIS
     Install modules for a new computer.
 .DESCRIPTION
-    Once PowerShell has been upgrade to at least version 5 this script will install the well-known
-    modules from PSGallery.
+    Once PowerShell has been upgraded to at least version 5 this script will install the well-known
+    modules from PSGallery and software packages from Chocolatey.
 .EXAMPLE
     C:\PS>c:\etc\script\new-workstation.ps1
     Simply runs this looped install-module process
@@ -22,44 +22,56 @@
     05.03.2017 JJK: Added chocolateyget provider as it helps with certain packages so streamlining
                     all chocolatey packages to use this provider.
 #>
-# Simple reporting
-"#------------------------------------------------------------------------------------------------------#"
-"`tYou are currently running PowerShell $($PSVersionTable.PSVersion)."
-"#------------------------------------------------------------------------------------------------------#"
-""
-$modules = "ImportExcel","psreadline","nameit","ScriptBrowser","posh-SSH","TreeSize"
-ForEach ($module in $modules){  # Loop through PowerShell modules
-    # Only using SkipPublisherCheck because I know these modules
-    if (!(get-module -Name $module)){
+[CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='Medium')]
+Param()
+Begin {
+    $modules = "ImportExcel","psreadline","nameit","ScriptBrowser","posh-SSH","TreeSize"
+    $packages = "sysinternals","visualstudiocode","vscode-powershell","SDelete"
+    # Simple reporting
+    "#------------------------------------------------------------------------------------------------------#"
+    "`tYou are currently running PowerShell $($PSVersionTable.PSVersion)."
+    "#------------------------------------------------------------------------------------------------------#"
+    ""
+}
+Process {
+    ForEach ($module in $modules){  # Loop through PowerShell modules
+        # Only using SkipPublisherCheck because I know these modules
+        if (!(get-module -Name $module)){
+            try{
+                install-module -Name $module -Scope AllUsers -Force -Confirm:$False -SkipPublisherCheck
+                "Module {0} installed" -f $module
+            }
+            Catch{
+                Write-error -Message "Unable to install $module" 
+            }
+            
+        }
+        Else {
+            "Module {0} already exists on this machine" -f $module
+        }
+    }
+    # Install Chocolatey
+    Install-PackageProvider chocolatey -Scope AllUsers -Confirm:$False -Force
+    # Adding this provider to handle chocolatey better
+    Find-PackageProvider ChocolateyGet -Verbose
+    Install-PackageProvider ChocolateyGet -Verbose -Force -Confirm:$False
+    Import-PackageProvider ChocolateyGet
+
+    ForEach ($package in $packages){   # Loop through Chocolatey packages
         try{
-            install-module -Name $module -Scope AllUsers -Force -Confirm:$False -SkipPublisherCheck
-            "Module {0} installed" -f $module
+            "Installing {0} package" -f $package
+            install-package -Name $package -confirm:$False -Force -ProviderName ChocolateyGet
         }
-        Catch{
-            Write-error -Message "Unable to install $module" 
+        catch {
+            "Unable to install {0} package via ChocolateyGet" -f $package
         }
-        
     }
-    Else {
-        "Module {0} already exists on this machine" -f $module
+    # use get-module not get-installedmodule if PowerCLI was installed traditionally
+    If(!(get-module -Name *vmware* -ListAvailable)){
+        "PowerCLI needs to be installed."
+        install-module vmware.powercli -force -AllowClobber -Scope AllUsers
     }
+    If(!(get-module -Name ActiveDirectory -ListAvailable)){"RSAT needs to be installed."}
+    # Install AD Powershell Module
+    . C:\etc\scripts\install-adpowershell.ps1
 }
-# Install Chocolatey
-Install-PackageProvider chocolatey -Scope AllUsers -Confirm:$False -Force
-# Adding this provider to handle chocolatey better
-Find-PackageProvider ChocolateyGet -Verbose
-Install-PackageProvider ChocolateyGet -Verbose -Force -Confirm:$False
-Import-PackageProvider ChocolateyGet
-$packages = "sysinternals","visualstudiocode","vscode-powershell","SDelete"
-ForEach ($package in $packages){   # Loop through Chocolatey packages
-    "Installing {0} package" -f $package
-    install-package -Name $package -confirm:$False -Force -ProviderName ChocolateyGet
-}
-# use get-module not get-installedmodule if PowerCLI was installed traditionally
-If(!(get-module -Name *vmware* -ListAvailable)){
-    "PowerCLI needs to be installed."
-    install-module vmware.powercli -force -AllowClobber -Scope AllUsers
-}
-If(!(get-module -Name ActiveDirectory -ListAvailable)){"RSAT needs to be installed."}
-# Install AD Powershell Module
-. C:\etc\scripts\install-adpowershell.ps1

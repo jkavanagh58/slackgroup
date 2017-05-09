cc9bb47d533b35987517b7c99aecf9bc24647171
John J Kavanagh <john.kavanagh@outlook.com>
2017-05-09 08:56:11 -0400

    new script - playing with SDelete

--- erase-recyclebin.ps1
+++ erase-recyclebin.ps1
@@ -14,5 +14,10 @@
     05.09.2017 JJK: Working with command line parameters
 #>
 $sdelete = "C:\Chocolatey\lib\sdelete.2.00\tools\sdelete.exe"
-$Recycler = (New-Object -ComObject Shell.Application).NameSpace(0xa)
-$Recycler.items() | foreach { . $sdelete -p 7 $_.path }
+if (test-path $sdelete){}
+    $Recycler = (New-Object -ComObject Shell.Application).NameSpace(0xa)
+    $Recycler.items() | foreach { . $sdelete -p 7 $_.path }
+}
+Else {
+    "Unable to find sdelete executable."
+}

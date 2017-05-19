a96f4fba0f4579631e43dada0613cc51f2557728
John J Kavanagh <john.kavanagh@outlook.com>
2017-05-19 11:01:48 -0400

    # Version 0.0.1

--- install-vscodeextensions.ps1
+++ install-vscodeextensions.ps1
@@ -1,25 +1,54 @@
+<#
+.SYNOPSIS
+	Short description
+.DESCRIPTION
+	Long description
+.EXAMPLE
+	C:\etc\scripts>.\install-vscodeextensions.ps1
+.NOTES
+	===========================================================================
+	Created with:	Visual Studio Code
+	Created on:		05.19.2017
+	Created by:		John Kavanagh
+	Organization:	TekSystems
+	Filename:		install-vscodeextensions.ps1
+	===========================================================================
+	05.19.2017 JJK: Removed wakatime as the install name does not match publisher provided name
+	05.19.2017 JJK: TODO: Add higher level process to check to see if vscode is installed
+	05.19.2017 JJK: TODO: need to handle already installed differently than unable to install 
+#>
 [CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='Low')]
 Param(
 	[array]$extArray,
-	[String]$curExt
+	[String]$curExt,
+	$isRunning = (get-process).Name -contains "code"
 )
 Begin{
+if ($host.Name -like "Visual Studio Code*"){
+	"This script will not run in the Visual Studio Code Integrated Terminal"
+	Break
+}
 # Configure vscode with suggested extensions
 # array of suggested extensions
 $extArray = @(
 	"denisgerguri.hunspell-spellchecker",
-	"WakaTime.vscode-wakatime",
+	"vscode-wakatime",
 	"Compulim.vscode-ipaddress",
-	"invalid"
+	"DougFinke.vscode-PSStackoverflow"
 )
-$curExt = invoke-express -command ".code --list-extensions"
+$curExt = invoke-expression -command "code --list-extensions"
 }
-$isRunning = (get-process)Name -contains "code")
 Process {
+# Just because it should be 
+Invoke-expression -command "code --enable-proposed-api powershell"
 If ($isRunning){
-	"Visual Studio Code is running and willl needs to be closed"
+	"Visual Studio Code is running and will be closed"
 	Try{
-		(get-process)
+		(get-process -Name "Code").kill()
+	}
+	Catch {
+		"Unable to close Visual Studio Code"
+		"In order for your workspace to show the new extension restart Visual Studio Code"
 	}
 }
 ForEach ($ext in $extArray){
@@ -27,14 +56,19 @@ ForEach ($ext in $extArray){
 		"{0} extension is already installed"
 	}
 	Else{
-		try {
-		. code --install-extension $ext
-		}
-		Catch {
-			$msg = "Unable to install {0}" -f $ext
-			write-warning -Message $msg
+		$instVal = invoke-expression -Command "code --install-extension $ext"
+		Switch -wildcard ($instVal){
+			"*successfully installed!"	{"{0} Installed" -f $ext}
+			"*returned 400"				{"{0} returned a 400 error" -f $ext}
+			"*already installed."		{"{0} is already installed" -f $ext}
+			default 					{"Return message {0}" -f $instval}	
 		}
 	}
+}
+} # End Process
+End{
+	"Restarting Visual Studio Code"
+	invoke-expression -Command "code"
 }
 
 

739fc08aa8a47837fb9bf40c81a4548b0b7c5f96
John J Kavanagh <john.kavanagh@outlook.com>
2017-05-22 18:17:30 -0400

    # Use ChocolateyGet provider as it seems to help with problem packages

--- new-chocolateyprovider.ps1
+++ new-chocolateyprovider.ps1
@@ -1,4 +1,4 @@
-# Use ChocolateyGet provider
+# Use ChocolateyGet provider as it seems to help with problem packages
 # https://github.com/jianyunt/ChocolateyGet
 Find-PackageProvider ChocolateyGet -verbose
 Install-PackageProvider ChocolateyGet -verbose

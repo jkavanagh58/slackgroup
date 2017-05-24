2eefae3633f816bd3c78c29463324a6fa27dc426
John J Kavanagh <john.kavanagh@outlook.com>
2017-05-24 19:40:49 -0400

    Working with a process to report on the vscode setup. First step installed extensions

--- get-vscodeinfo.ps1
+++ get-vscodeinfo.ps1
@@ -30,7 +30,34 @@
     03.21.2017 JJK: Filename pattern is not consistent, will look for a regex way
     03.21.2017 JJK: TODO: use REST to get a list of new and updated extensions
 #>
-
+# Determine if vscode and/or insiders is installed
+If (test-path (Join-Path -Path $env:USERPROFILE -ChildPath ".vscode-insiders\extensions")) {
+    $insiders = $True
+    $insCmdline = "code - insiders"
+    $vscodeinsExt = invoke-expression -command "$insCmdline --list-extensions"
+}
+Else {
+    $insiders = $False
+}
+If (test-path (Join-Path -Path $env:USERPROFILE -ChildPath ".vscode\extensions")) {
+    $vscode = $True
+    $vscodeExt = invoke-expression -command "code --list-extensions"
+}
+If ($Insiders -eq $False -AND $vscode -eq $False) {
+    "No version of Visual Studio Code detected"
+    Break
+}
+"Visual Studio Code Extensions ({0}):" -f $vscodeExt.count
+ForEach ($ext in $vscodeExt){
+    "`t$ext"
+}
+If ($vscodeinsExt){
+    "Visual Studio Code - Insiders Extensions ({0}):" -f $vscodeinsExt.count
+    ForEach ($ext in $vscodeinsExt){
+        "`t$ext"
+    }
+}
+<#
 # List the installed extensions
 $extPath = Join-Path -Path $env:USERPROFILE -ChildPath ".vscode-insiders\extensions"
 if (test-path $extPath){
@@ -38,3 +65,4 @@ if (test-path $extPath){
     "You have {0} installed extensions" -f $varExtensions.count
     $varExtensions | sort-object -Property Name | select-object -Property @{n="Extension";e={$_.Name.split("-")[0]}}, @{n="Version";e={$_.Name.split("-")[1]}}
 }
+#>

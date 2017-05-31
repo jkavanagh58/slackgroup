e8c63046c619886362fe8eb88fbd60bb5024ab7b
John J Kavanagh <john.kavanagh@outlook.com>
2017-05-31 09:38:40 -0400

    added .Net method solution if no access to resolve-dnsname cmdlet

--- pingsweep-oneliners.ps1
+++ pingsweep-oneliners.ps1
@@ -1,10 +1,13 @@
 # old version 
-# Limitations... client machine WMI does not return data but this is a one-liner
+# Limitations... multiple reasons why WMI might not respond
+# but this is just a quick sweep
 1..254 | %{ if (test-connection -Computername "172.22.65.$_" -Count 1 -ErrorAction SilentlyContinue){
 	If ($var = gwmi win32_operatingsystem -Computername "172.22.65.$_" -ErrorAction SilentlyContinue){
 		$var.caption
 	}
 }
 }
-# Less network traffic with this one as it is a DNS lookup
-1..254 | %{"172.22.65.$_";(Resolve-DnsName "172.22.65.$_" -ErrorAction SilentlyContinue).NameHost} | out-file c:\etc\65subnet.txt
+# Less network traffic with this one as it is a DNS lookup using your machine 
+1..254 | %{"172.22.65.$_";(Resolve-DnsName "172.22.65.$_" -ErrorAction SilentlyContinue).NameHost} 
+# Use .Net 
+1..254 | %{"172.22.65.$_";Try{"`t$([system.net.dns]::getHostByAddress("172.22.65.$_").HostName)"}Catch{"`tNo Data Returned"}}

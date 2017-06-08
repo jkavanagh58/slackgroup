3671e3d497c5468acdfc964cba6cb15012ee4865
John J Kavanagh <john.kavanagh@outlook.com>
2017-06-08 09:31:35 -0400

    Working with File Open Dialog box

--- sign-psfile.ps1
+++ sign-psfile.ps1
@@ -22,15 +22,36 @@
 #>
 [CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='Medium')]
 Param( 
-    [Parameter(ValueFromPipeline=$true, Mandatory=$true)]
+    [Parameter(ValueFromPipeline=$true, Mandatory=$false)]
     [Alias("ScriptFile")]
     [String]$sFile 
 )
+# Show an Open File Dialog and return the file selected by the user.
+function Read-OpenFileDialog([string]$WindowTitle, [string]$InitialDirectory, [string]$Filter = "All files (*.*)|*.*", [switch]$AllowMultiSelect) {  
+    Add-Type -AssemblyName System.Windows.Forms
+    $openFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $openFileDialog.Title = $WindowTitle
+    if (![string]::IsNullOrWhiteSpace($InitialDirectory)) { $openFileDialog.InitialDirectory = $InitialDirectory }
+    $openFileDialog.Filter = $Filter
+    if ($AllowMultiSelect) { $openFileDialog.MultiSelect = $true }
+    $openFileDialog.ShowHelp = $true    # Without this line the ShowDialog() function may hang depending on system configuration and running from console vs. ISE.
+    $openFileDialog.ShowDialog() > $null
+    if ($AllowMultiSelect) { return $openFileDialog.Filenames } else { return $openFileDialog.Filename }
+}
+$sFile = Read-OpenFileDialog -WindowTitle "Select the script to be Digitally Signed" -InitialDirectory 'C:\' -Filter "Script files (*.ps1)|*.ps1"
+if (![string]::IsNullOrEmpty($filePath)) { "You selected {0} to be digitally signed" -f $sFile}
+else { "You did not select a file." }
 # If file entered on commandline exists digitally sign    
 if (test-path $sFile){
     "Signing $sFile"
     # Select the Certificate designate for CodeSigning
-    $cert = @(Get-ChildItem cert:\CurrentUser\Root -codesigning)[0]
+    try {
+        $cert = @(Get-ChildItem cert:\CurrentUser\Root -codesigning)[0]
+    }
+    Catch {
+        "No Appropriate Certificate was found."
+        Exit
+    }
     # Sign file 
     Set-AuthenticodeSignature $sFile $cert
 }

6be3c3e51aa57fa117d29e6c7e045ef1666954ce
John J Kavanagh <john.kavanagh@outlook.com>
2017-06-29 11:23:54 -0400

    collection of eventlog powershell one-liners

---
+++ eventlog-oneliners.ps1
@@ -0,0 +1,2 @@
+# Get Logon Type 10 entries for the last day
+(get-eventlog -LogName Security -after ((get-date).AddDays(-1))).where{$_.InstanceID -eq "4624" -Or $_.InstanceID -eq "4634" -and $_.ReplacementStrings[8] -eq "10"} 

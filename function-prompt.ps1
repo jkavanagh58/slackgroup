95c56d16791d968bbf5093aef213f8d26c92815a
John J Kavanagh <john.kavanagh@outlook.com>
2017-07-03 10:54:41 -0400

    removed window title

--- function-prompt.ps1
+++ function-prompt.ps1
@@ -16,7 +16,6 @@ Function Prompt {
 Function Prompt {
 	# Needs work
 	" "
-	$host.ui.RawUI.WindowTitle = "Wegmans - TechWintel Automation"
 	Write-Host "I " -NoNewline; Write-Host "$([char]9829) " -ForegroundColor Red -NoNewline; Write-Host "vscode " -NoNewline
     Write-Host " >_" -NoNewline -ForegroundColor Yellow
 }

691f95980a7313c2f001be95260aa923f515aa4d
John J Kavanagh <john.kavanagh@outlook.com>
2017-07-03 09:27:44 -0400

    added the prompt function I have for vscode.....

--- function-prompt.ps1
+++ function-prompt.ps1
@@ -12,3 +12,11 @@ Function Prompt {
 	}
 	" "
 }
+# I have this in my Microsoft.PowerShellISE_profile.ps1
+Function Prompt {
+	# Needs work
+	" "
+	$host.ui.RawUI.WindowTitle = "Wegmans - TechWintel Automation"
+	Write-Host "I " -NoNewline; Write-Host "$([char]9829) " -ForegroundColor Red -NoNewline; Write-Host "vscode " -NoNewline
+    Write-Host " >_" -NoNewline -ForegroundColor Yellow
+}

f303277688a7af384cae4e8ef1b3e6253d6dd3b8
John J Kavanagh <john.kavanagh@outlook.com>
2017-07-03 10:06:52 -0400

    Splatted the first example

--- eventlog-oneliners.ps1
+++ eventlog-oneliners.ps1
@@ -1,2 +1,9 @@
 # Get Logon Type 10 entries for the last day
 (get-eventlog -LogName Security -after ((get-date).AddDays(-1))).where{$_.InstanceID -eq "4624" -Or $_.InstanceID -eq "4634" -and $_.ReplacementStrings[8] -eq 10}
+# Splatted just to be cleaner
+$logParams = @{
+	LogName = "Security"
+	InstanceID = "4624","4634"
+	After = (get-date).AddDays(-1)
+}
+get-eventlog @logParams | where-Object {$_.ReplacementStrings[8] -eq 10}

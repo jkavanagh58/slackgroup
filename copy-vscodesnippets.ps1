ca98a2e8cb7318fb0438b5c7d008c938a2aee5ea
John J Kavanagh <john.kavanagh@outlook.com>
2017-07-27 16:23:34 -0400

    trying to strip html from download

--- copy-vscodesnippets.ps1
+++ copy-vscodesnippets.ps1
@@ -1,14 +1,24 @@
 [CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='Medium')]
 Param(
-	[String]$snippetFolder = Join-Path -Path $env:USERPROFILE -ChildPath "\AppData\Roaming\Code\User\Snippets",
+	[String]$snippetFolder,
 	[String]$url = "https://github.com/jkavanagh58/slackgroup/blob/master/Snippets/vscode/powershell.json"
 )
+Begin{
+$snippetFolder = Join-Path -Path $env:USERPROFILE -ChildPath "\AppData\Roaming\Code\User\Snippets"
 # Archive existing Snippet folder for rollback
 If (Test-Path -Path "$snippetFolder\Powershell.json" -PathType Leaf){
 	"Archiving current Snippet File"
 	Copy-Item -Path "$snippetFolder\PowerShell.json" -Destination "$snippetFolder\Powershell.archive" -Force
 }
+}
+Process {
 # Copy file from github repo
 $webclient = New-Object System.Net.WebClient
-$file = "$snippetFolder\powershell.json"
-$webclient.DownloadFile($url,$file)
+$file = "c:\etc\badjjk.json"
+# $webclient.DownloadString($url,$file)
+$webclient.DownloadData($url)
+}
+End {
+	Remove-Variable -Name snippetFolder, url, file
+	[System.GC]::Collect()
+}

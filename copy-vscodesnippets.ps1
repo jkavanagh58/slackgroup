d9b854cff68c18cd000a5dcd4d45ea9f83633694
John J Kavanagh <john.kavanagh@outlook.com>
2017-12-12 19:32:18 -0500

    adding comment for next update to include Insiders inclusion

--- copy-vscodesnippets.ps1
+++ copy-vscodesnippets.ps1
@@ -1,3 +1,34 @@
+<#
+.SYNOPSIS
+	Short description
+.DESCRIPTION
+	Long description
+.PARAMETER Path
+	Specifies a path to one or more locations.
+.PARAMETER LiteralPath
+	Specifies a path to one or more locations. Unlike Path, the value of LiteralPath is used exactly as it
+	is typed. No characters are interpreted as wildcards. If the path includes escape characters, enclose
+	it in single quotation marks. Single quotation marks tell Windows PowerShell not to interpret any
+	characters as escape sequences.
+.PARAMETER InputObject
+	Specifies the object to be processed.  You can also pipe the objects to this command.
+.EXAMPLE
+	C:\PS>
+	Example of how to use this cmdlet
+.EXAMPLE
+	C:\PS>
+	Another example of how to use this cmdlet
+.INPUTS
+	Inputs to this cmdlet (if any)
+.OUTPUTS
+	Output from this cmdlet (if any)
+.NOTES
+	File Name: copy-vscodesnippets.ps1
+	Date Created: 2017-12-12 19:30:19
+	Author: Kavanagh, John J.
+	General notes
+	12.12.2017 JJK:	Need method for handling Insiders version
+#>
 [CmdletBinding(SupportsShouldProcess=$true,ConfirmImpact='Medium')]
 Param(
 	[String]$snippetFolder,

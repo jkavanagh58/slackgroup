3036d49e72260568eaf9e9b521e74237dbd1dd00
John J Kavanagh <john.kavanagh@outlook.com>
2018-03-16 11:01:59 -0400

    Quick function to list any outdated installed modules

---
+++ get-updatedmodules.ps1
@@ -0,0 +1,32 @@
+Function new-modules {
+    <#
+        Quick module to simply list any module you have installed
+        that is not the current version.
+        NOTE: For easy access place this function in your PowerShell profile for easy access.
+    #>
+    [CmdletBinding()]
+    Param (
+        [System.Array]$varModules = (get-installedmodule)
+    )
+    BEGIN {
+
+
+    }
+
+    PROCESS{
+        $i=0
+        ForEach ($curModule in $varModules | Sort-Object -Property Name) {
+            $i++    # Progress counter
+            Write-Progress -Activity "Checking Module" -CurrentOperation $curModule.Name -PercentComple ($i/$varModules.count*100)
+            If ($curModule.Version -lt (find-module -Name $curModule.Name).Version) {
+                "{0} module is currently out of date" -f $curModule.Name
+            }
+        }
+    }
+    END {
+        Remove-Variable -Name varModules, i
+        [System.GC]::Collect()
+    }
+}
+
+ 

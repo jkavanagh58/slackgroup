9c4e0d78f184f17c257b0b0d424bd6780cf39e09
John J Kavanagh <john.kavanagh@outlook.com>
2018-04-18 18:39:21 -0400

    very basic testing

--- Pester/test-script.test.ps1
+++ Pester/test-script.test.ps1
@@ -1,10 +1,15 @@
 D:\tools\GitHub\slackgroup\Pester\test-script.ps1
 
 Describe 'Simple Test' {
-    it 'Validate Path Parameter' {
-        $path | Should -Exist
-    }
-    It 'Path exists' {
-        test-path $path | Should -Be $True
+    Context 'Validate Folder Collection Process' {
+        it 'Validate Path Parameter' {
+            $path | Should -Exist
+        }
+        It 'Path exists' {
+            test-path $path | Should -Be $True
+        }
+        It 'List of folders was created' {
+            $tFiles.count | Should -BeGreaterThan 0
+        }
     }
 }

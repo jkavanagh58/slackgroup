f489e6d0e659aa33f7e3a382a9d09f3b8c8fa579
John J Kavanagh <john.kavanagh@outlook.com>
2018-05-20 12:51:06 -0400

    Added gathering all local drives for thorough repo get

--- get-githistory.ps1
+++ get-githistory.ps1
@@ -1,42 +1,42 @@
 #Requires -Modules ImportExcel
 <#
-.SYNOPSIS
-	Report on git repo activity
-.DESCRIPTION
-	Retrieves the git history of a repo using a custom format. Returned data is written to a PSCustomObject.
-.PARAMETER repoPath
-	Specifies a path of the cloned repo
-.PARAMETER gitSep
-	Specifies the seperator character for formating git prety output and then use when creating array objects
-.EXAMPLE
-	C:\PS> .\get-githistory.ps1
-	Example of how to use this script
-.INPUTS
-	Inputs to this cmdlet (if any)
-.OUTPUTS
-	System.Management.Automation.PSCustomObject
-.NOTES
-	===========================================================================
-	Created with:	Visual Studio Code
-	Created on:		05.11.2018
-	Created by:		Kavanagh, John J.
-	Organization:	TEKSystems
-	Filename:		get-githistory.ps1
-	===========================================================================
-	05.12.2018 JJK: Changed git log output field seperator as commas had been enteterd in commit message
-	05.12.2018 JJK: Added custom help so script can be documented as initial script debugged successfully
-	05.12.2018 JJK: TODO: Develop better process for setting location to a valid repo path
-	05.12.2018 JJK: Testing a process to find local repos for better felxibility, use for parameter validation?
-	05.12.2018 JJK: TODO: Use Repos for parameter validation or other presentation at runtime
-	05.12.2018 JJK: Removed fully typed field seperator from git pretty and the PSCustom object. Making flexible
-					as data entry in commit message could be problematic, for example I use colons and comments so
-					I set the gitsep to a semicolon.
-	05.14.2018 JJK:	DONE: Add reporting using the ImportExcelModule - working on multiple sheets.
-	05.14.2018 JJK:	TODO: Approach reporting with new info git log is repo specific not computer specific
-	05.14.2018 JJK:	DONE: Look for git date value - use get-Date to make commit Date easier to read
-	05.14.2018 JJK:	Tested new Report format; converted commandling to splat
-	05.17.2018 JJK:	Sterilized Report path to current users documents folder.
-	05.17.2018 JJK: TODO: Use CIM to create a list of physical disks (I believe local clones can only be in physical)
+	.SYNOPSIS
+		Report on git repo activity
+	.DESCRIPTION
+		Retrieves the git history of a repo using a custom format. Returned data is written to a PSCustomObject.
+	.PARAMETER repoPath
+		Specifies a path of the cloned repo
+	.PARAMETER gitSep
+		Specifies the seperator character for formating git prety output and then use when creating array objects
+	.EXAMPLE
+		C:\PS> .\get-githistory.ps1
+		Example of how to use this script
+	.INPUTS
+		Inputs to this cmdlet (if any)
+	.OUTPUTS
+		System.Management.Automation.PSCustomObject
+	.NOTES
+		===========================================================================
+		Created with:	Visual Studio Code
+		Created on:		05.11.2018
+		Created by:		Kavanagh, John J.
+		Organization:	KavanaghTech
+		Filename:		get-githistory.ps1
+		===========================================================================
+		05.12.2018 JJK: Changed git log output field seperator as commas had been enteterd in commit message
+		05.12.2018 JJK: Added custom help so script can be documented as initial script debugged successfully
+		05.12.2018 JJK: TODO: Develop better process for setting location to a valid repo path
+		05.12.2018 JJK: Testing a process to find local repos for better felxibility, use for parameter validation?
+		05.12.2018 JJK: TODO: Use Repos for parameter validation or other presentation at runtime
+		05.12.2018 JJK: Removed fully typed field seperator from git pretty and the PSCustom object. Making flexible
+						as data entry in commit message could be problematic, for example I use colons and comments so
+						I set the gitsep to a semicolon.
+		05.14.2018 JJK:	DONE: Add reporting using the ImportExcelModule - working on multiple sheets.
+		05.14.2018 JJK:	TODO: Approach reporting with new info git log is repo specific not computer specific
+		05.14.2018 JJK:	DONE: Look for git date value - use get-Date to make commit Date easier to read
+		05.14.2018 JJK:	Tested new Report format; converted commandling to splat
+		05.17.2018 JJK:	Sterilized Report path to current users documents folder.
+		05.17.2018 JJK: TODO: Use CIM to create a list of physical disks (I believe local clones can only be in physical)
 #>
 [CmdletBinding()]
 Param (
@@ -65,7 +65,9 @@ Param (
 BEGIN {
 	Try {
 		Write-Verbose "[BEGIN]Getting locally available Respositories"
-		$gitRepos = Get-Childitem .git -Path c:\ -Recurse -Directory -Hidden -ErrorAction SilentlyContinue 
+		# List of local drives
+		$ldrives = (Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType = 3").ForEach{Join-Path $_.DeviceID -ChildPath "\"}
+		$gitRepos = Get-Childitem .git -Path $ldrives -Recurse -Directory -Hidden -ErrorAction SilentlyContinue 
 		$gitRepos | select-Object -Property Parent, LastAccessTime, FullName
 	}
 	Catch {

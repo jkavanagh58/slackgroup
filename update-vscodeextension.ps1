c37951fec4e3329460b9771726551ae8a7b9a806
John J Kavanagh <john.kavanagh@outlook.com>
2018-07-27 17:47:59 -0400

    Included start of formal code example
    Included start of formal code example
    Uses a reference computer

--- update-vscodeextension.ps1
+++ update-vscodeextension.ps1
@@ -30,3 +30,46 @@ code --install-extension gerane.Theme-Blackboard
 (get-process).where{$_.ProcessName -eq "code" -Or $_.ProcessName -eq "Code - Insiders"}.Foreach{$_.kill()};code-insiders
 (get-process).where{$_.ProcessName -eq "code" -Or $_.ProcessName -eq "Code - Insiders"}.Foreach{$_.kill()};code
 
+# Possible way to use it in a scripted fashion
+Function update-newvscode {
+[CmdletBinding()]
+param (
+	[System.Array]$curExtensions,
+	[System.String]$refMachine = "INFO-67TVF72",
+	[System.Array]$refExtensions,
+	[System.String]$codeCommand = (get-command -Name Code*).Name.Replace(".cmd",""),
+	$needExtensions
+)
+BEGIN {
+	If ($codeCommand.Count -gt 1){
+		Write-Error -Message "Hey I said this was a quick example"
+		Write-Error -Message "Pick one version or the other I am not working on two"
+		start-sleep -Seconds 2
+		EXIT
+	}
+	$curExtensions = invoke-command -ScriptBlock {powershell -command "$codeCommand --list-extensions"}
+	$invokeCommandSplat = @{
+		ComputerName = $refMachine
+		ScriptBlock  = {powershell -nologo -noprofile -Command "$using:codeCommand --list-extensions"}
+	}
+	$refExtensions = Invoke-Command @invokeCommandSplat
+}
+PROCESS {
+	$needExtensions = compare-object -ReferenceObject $refExtensions -DifferenceObject $curExtensions
+	ForEach ($thisExtension in $needExtensions){
+		$thisExtension.InputObject
+		$installCmd = "$codeCommand --install-extension $thisExtension.InputObject"
+		Write-Verbose -Message "[PROCESS]Installing "
+		Try {
+			"Invoke-command -ScriptBlock {$installCmd}"
+		}
+		Catch {
+			Write-Error -Message "Unable to install $($thisExtension.InputObject) extension"
+		}
+	}
+}
+END {
+	Remove-Variable -Name curExtensions, refExtensions, needExtensions, invokeCommandSplat
+	[System.GC]::Collect()
+}
+}

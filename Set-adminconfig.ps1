88877054dbc94f8308a7f0c5758c91949b8eed89
John J Kavanagh <john.kavanagh@outlook.com>
2018-08-21 13:10:05 -0400

    Admin New desktop Config
    Added function for enabling RDP
    Added function for enabling Hyper-V (lab purposes only) 
    Need to document post-work for Hyper-V

---
+++ Set-adminconfig.ps1
@@ -0,0 +1,37 @@
+Function Enable-RDPAccess {
+[CmdletBinding()]
+Param (
+
+)
+    # Enable RDP
+    $setItemPropertySplat = @{
+        Path = 'HKLM:SystemCurrentControlSetControlTerminal Server'
+        Name = "fDenyTSConnections"
+        Value = 0
+    }
+    set-ItemProperty @setItemPropertySplat
+    # Open Firewall Rule for Remote Desktop
+    Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+    # Enforce Secure Connections
+    $setItemPropertySplat = @{
+        Path = 'HKLM:SystemCurrentControlSetControlTerminal ServerWinStationsRDP-Tcp'
+        Name = "UserAuthentication"
+        Value = 1
+    }
+    set-ItemProperty @setItemPropertySplat
+}
+Function Enable-WinHyperV {
+[cmdletBinding()]
+Param (
+
+)
+    $hdwrCheck = Get-CimInstance -ClassName win32_processor -Property Name, SecondLevelAddressTranslationExtensions, VirtualizationFirmwareEnabled, VMMonitorModeExtensions 
+    If ($hdwrCheck.SecondLevelAddressTranslationExtensions){
+        Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+    }
+    Else {
+        Write-Error -Message "Unable to enable Hyper-V; SLAT is not enabled."
+        Exit
+    }
+
+}

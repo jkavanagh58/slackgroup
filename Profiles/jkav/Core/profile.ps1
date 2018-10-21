cfe405ee7c23cbe0ffe3c56936986d9b9de2dbb7
John J Kavanagh <john.kavanagh@outlook.com>
2018-10-21 11:39:51 -0400

    Profiles Collection Base Profile for PowerShell Core

---
+++ Profiles/jkav/Core/profile.ps1
@@ -0,0 +1,27 @@
+Function Prompt {
+	$admRole = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+	If ($psversiontable.psversion -lt 6){
+		If ($admRole) {
+			$host.ui.RawUI.WindowTitle = "Kavanaghtech (Admin)"
+			Write-Host "I " -NoNewline; Write-Host "$([char]9829) " -ForegroundColor Red -NoNewline; Write-Host "PS # " -NoNewline
+			return ' '
+		}
+		Else {
+			$host.ui.RawUI.WindowTitle = "KavanaghTech"
+			Write-Host "I " -NoNewline; Write-Host "$([char]9829) " -ForegroundColor Red -NoNewline; Write-Host " PS >_ " -NoNewline
+			return ' '
+		}
+	}
+	ElseIf ($psversiontable.psversion -ge 6){
+		If ($admrole){
+			$host.ui.RawUI.WindowTitle = "KavanaghTech (Admin)"
+			Write-Host "I " -NoNewline; Write-Host "$([char]9829) " -ForegroundColor Red -NoNewline; Write-Host "sudo # " -NoNewline
+			return ' '
+		}
+		Else {
+			$host.ui.RawUI.WindowTitle = "KavanaghTech"
+			Write-Host "I " -NoNewline; Write-Host "$([char]9829) " -ForegroundColor Red -NoNewline; Write-Host "Core >_ " -NoNewline
+			return ' '
+		}
+	}
+}

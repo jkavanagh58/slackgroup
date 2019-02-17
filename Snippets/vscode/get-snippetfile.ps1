b9093112ba98196f8d627ac468ca7839a5a28ba0
John J Kavanagh <john.kavanagh@outlook.com>
2019-02-17 10:33:40 -0500

    Snippets: VSCode Debugging script to allow users to download the provided snippet file.

--- Snippets/vscode/get-snippetfile.ps1
+++ Snippets/vscode/get-snippetfile.ps1
@@ -1,12 +1,19 @@
 # Needs CBH
 [CmdletBinding()]
 Param (
-    $snippetURL = "https://gist.github.com/jkavanagh58/9312bd0d78eddc54d51998c0a5c9831a"   
+    [parameter(Mandatory=$False, ValueFromPipeline=$True,
+        HelpMessage = "URL For the Sample VSCode PowerShell Snippet File")]
+    [System.String]$snippetURL = "https://gist.github.com/jkavanagh58/9312bd0d78eddc54d51998c0a5c9831a",
+    [parameter(Mandatory=$False, ValueFromPipeline=$False,
+        HelpMessage = "VSCode Folder location for User Snippets")]
+    [System.String]$snippetFolder,
+    [parameter(Mandatory=$False, ValueFromPipeline=$False,
+        HelpMessage = "VSCode Folder location for User Snippets")]
+    [System.String]$snippetFile
 )
 BEGIN {
-    [System.String]$snippetFolder = Join-Path $env:APPDATA -ChildPath "\code\User\snippets"
-    [System.String]$snippetFile = Join-Path $snippetFolder -ChildPath "powershell.json"
-    
+    $snippetFolder = Join-Path $env:APPDATA -ChildPath "\code\User\snippets"
+    $snippetFile = Join-Path $snippetFolder -ChildPath "powershell.json"
     If (test-path $snippetFile){
         # Backup the existing Snippet file if it exists
         $snippetBackup = Join-Path $snippetFolder -ChildPath "powershell.bak"
@@ -17,9 +24,14 @@ PROCESS {
     Try {
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
         $snip = Invoke-WebRequest -Uri $snippetURL -OutFile $snippetFile -PassThru
+        $snip
     }
     Catch {
         $Error[0].Exception.Message
     }
 
+}
+END {
+    Remove-Variable -Name snip*
+    [System.GC]::Collect()
 }

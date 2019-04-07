4da7c9f2a11d68c6eef47a6ee7165211addf71e5
John Kavanagh <jkavpowershell@outlook.com>
2019-04-07 12:35:24 -0400

    OneLiners: Install PowerShell Core Simple example for installing PowerShell Core The link includes some handy parameters as well NOTE: Link also includes method for installing on most Linux distros

---
+++ install-psCore.ps1
@@ -0,0 +1,6 @@
+<#
+    .LINK https://www.thomasmaurer.ch/2019/03/how-to-install-and-update-powershell-6/
+        Great reference for a way to install and update PowerShell Core; Link also 
+        includes a solid reference to run it on most linux distros
+#>
+iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"

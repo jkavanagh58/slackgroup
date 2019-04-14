3a449206d3e4c1de9135c0f313c1d9084e949f87
John J Kavanagh <john.kavanagh@outlook.com>
2019-04-14 09:04:04 -0400

    Shell: rasbpi update Added auto responder

--- sysupdate.sh
+++ sysupdate.sh
@@ -2,9 +2,9 @@
 echo "UPDATING SYSTEM SOFTWARE – UPDATE"
 apt-get update
 echo "UPDATING SYSTEM SOFTWARE – UPGRADE"
-apt-get upgrade
+apt-get -y upgrade
 echo "UPDATING SYSTEM SOFTWARE – DISTRIBUTION"
-apt-get dist-upgrade
+apt-get -y dist-upgrade
 echo "REMOVING APPLICATION ORPHANS"
 apt-get autoremove –purge
 echo "UPDATING FIRMWARE"

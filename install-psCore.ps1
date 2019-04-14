445b547816161b49f5fcbaf0250bccfbc834bb06
John Kavanagh <jkavpowershell@outlook.com>
2019-04-14 15:25:03 -0400

    Tips: Install Core Changed the iex to Invoke-Command to comply with InjectionHunter

--- install-psCore.ps1
+++ install-psCore.ps1
@@ -3,4 +3,6 @@
         Great reference for a way to install and update PowerShell Core; Link also 
         includes a solid reference to run it on most linux distros
 #>
-iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
+# iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
+$installCore = "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
+Invoke-Command -Command $installCore

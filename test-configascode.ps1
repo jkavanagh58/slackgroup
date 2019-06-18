8934bdfdad984c0c18a65f33e1558051ba240ca4
John Kavanagh <john_kavanagh@docupace.com>
2019-06-18 14:11:37 -0400

    ConfifAsCode Added tests for git Added tests for PowerShell

--- test-configascode.ps1
+++ test-configascode.ps1
@@ -9,3 +9,23 @@ Describe "Script Folder" {
             Should -BeGreaterThan 6524749824
     }
 }
+Describe "Git is installed" {
+    It "Check for cmd" {
+        $varGit = get-Command -Name "git.exe"
+        $varGit | Should Be $True
+        $varGit.Version |
+            Should -BeGreaterThan ([version]'1.0.0.1')
+    }
+}
+Describe "PowerShell" {
+    $installedModules = Get-InstalledModule
+    It "Check for WMF 5.1" {
+        $psversiontable.psversion |
+            Should -BeGreaterThan ([version]'5.0.0.0')
+    }
+    It "Check for PSSA" {
+       Compare-Object "PSScriptAnalyzer" $installedModules |
+        Should -BeTrue
+    }
+
+}

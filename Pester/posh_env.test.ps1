c7d6f734d3f03ac36909e3af93ee914ed03139d5
JKav <JKavpowershell@outlook.com>
2019-08-13 13:22:57 -0400

    Tests: PoSH Env Working through tests for 5.x environment Test version and paths Test for optional elements

--- Pester/posh_env.test.ps1
+++ Pester/posh_env.test.ps1
@@ -3,6 +3,12 @@ describe 'PoSH Environment' {
     it 'PowerShell Version' {
         $psversiontable.PSVersion.Major | should be 5
     }
+    it 'Local scripts ' {
+        Test-Path -Path c:\etc\scripts | Should -Be $True
+    }
+    it 'Old school profile' {
+        Test-Path $profile.CurrentUserAllHosts | Should -Be $True
+    }
 
     Context "Optional Elements" {
         # would require test to be Runas Admin
@@ -12,6 +18,14 @@ describe 'PoSH Environment' {
         it 'Visual Studio Code' {
             $cmds = get-command code* -CommandType 'Application'
             $cmds.count | Should -BeGreaterOrEqual 1
+        }
+        it 'Git Installed' {
+            $gitcmd = get-command -Name git.exe -CommandType Application
+            $gitcmd.count | Should -BeGreaterOrEqual 1
+        }
+        it 'Editor Services Modules' {
+            $pses = get-installedmodule -Name EditorServices* 
+            $pses.count | Should -BeGreaterThan 1
         }
     }
 	

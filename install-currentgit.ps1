bcf4ad5d3488d55810a697bc12770ab644fbb6df
JKav <JKavpowershell@outlook.com>
2019-10-13 12:45:03 -0400

    Utils: Install Update git Placeholder Use Chocolatey to install most current version of git

---
+++ install-currentgit.ps1
@@ -0,0 +1,67 @@
+#Requires -Module Chocolatey
+<#
+.SYNOPSIS
+	Install git
+.DESCRIPTION
+	Use Chocolatey to install git for local computer. Determines install versions in choco folder
+	and cleans up. Post cleanup runs install based on processor avail.
+.PARAMETER chocoInstallPath
+	Specifies a path to one or more locations.
+.PARAMETER chocoInstallVersion
+	Specifies a path to one or more locations. Unlike Path, the value of LiteralPath is used exactly as it
+	is typed. No characters are interpreted as wildcards. If the path includes escape characters, enclose
+	it in single quotation marks. Single quotation marks tell Windows PowerShell not to interpret any
+	characters as escape sequences.
+.PARAMETER procAvail
+	Specifies the object to be processed.  You can also pipe the objects to this command.
+.EXAMPLE
+	PS>_
+	Example of how to use this cmdlet
+.EXAMPLE
+	PS>_
+	Another example of how to use this script.
+.INPUTS
+	Inputs to this cmdlet (if any)
+.OUTPUTS
+	Output from this cmdlet (if any)
+.NOTES
+	===========================================================================
+	Created with:	Visual Studio Code
+	Created on:		10.13.2019
+	Created by:		John J. Kavanagh
+	Organization:	organization
+	Filename:		Untitled-1
+	===========================================================================
+	10.13.2019 JJK:	TODO: Determine currently installed version
+	10.13.2019 JJK:	TODO: Handle git and git.install folders
+#>
+[CmdletBinding()]
+Param (
+	[parameter(Mandatory = $False, ValueFromPipeline = $False,
+		HelpMessage = "HelpMessage")]
+	[Int32]$procAvail = ([System.IntPtr]::Size) * 8,
+	[parameter(Mandatory = $False, ValueFromPipeline = $False,
+		HelpMessage = "Count of current git installs")]
+	[Int32]$chocoInstallPaths = (Get-ChildItem git.install.* -Path C:\Chocolatey\lib -Directory | Measure-Object).Count
+)
+BEGIN {
+	Function clear-gitinstall {
+		<#
+			.SYNOPSIS
+				Remove previous install folders
+		#>
+		[CmdletBinding()]
+		Param(
+			[parameter(Mandatory = $False, ValueFromPipeline = $False,
+				HelpMessage = "Currently installed version, used to remo")]
+			[VariableType]$VariableName
+		)
+
+	}
+}
+PROCESS {
+
+}
+END {
+
+}

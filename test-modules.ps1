cc4196a5d01222436dc0d419c65225e9fc046e7f
Kavanagh, John J <jkavpowershell@outlook.com>
2019-10-27 12:48:46 -0400

    Sys: Module Version Check Added update swith parameter Not implemented

--- test-modules.ps1
+++ test-modules.ps1
@@ -1,9 +1,23 @@
-$curModules = get-installedmodule | Sort-Object -Property Name
-ForEach ($varModule in $curModules) {
-    Write-Progress -Activity "Inspecting Module $($varModule.Name)" -Status "Version Check" 
-    [Version]$versionInstalled = $varModule.Version
-    [Version]$versionOnline = (Find-Module -Name $varModule.Name).Version
-    If ($versionOnline -gt $versionInstalled) {
-        "`t{0} is due for an update" -f $varModule.Name
+[CmdletBinding()]
+Param (
+    [parameter(Mandatory=$False, ValueFromPipeline=$True)
+            HelpMessage = "Perform Update")]
+    [Swith]$UpdateModule
+)
+BEGIN {
+    $curModules = get-installedmodule | Sort-Object -Property Name
+}
+PROCESS {
+    ForEach ($varModule in $curModules) {
+        Write-Progress -Activity "Inspecting Module $($varModule.Name)" -Status "Version Check" 
+        [Version]$versionInstalled = $varModule.Version
+        [Version]$versionOnline = (Find-Module -Name $varModule.Name).Version
+        If ($versionOnline -gt $versionInstalled) {
+            "`t{0} is due for an update" -f $varModule.Name
+        }
     }
+}
+END {
+    Remove-Variable -Name versionInstalled, versionOnline, curModules
+    [System.GC]::Collect()
 }

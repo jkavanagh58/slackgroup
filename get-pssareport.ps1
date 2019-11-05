aa2c1184c77ea3d77250bd27d1ce53ff73cd813f
JKav <JKavpowershell@outlook.com>
2019-11-05 12:56:40 -0500

    Utils: PSSA Report Cleaned up code. Added extension validation for the xlFile parameter Cleaned up parameter names Implemented option for reporting errors only

--- get-pssareport.ps1
+++ get-pssareport.ps1
@@ -1,6 +1,44 @@
+#Requires -Module ImportExcel, PSScriptAnalyzer
+<#
+.SYNOPSIS
+	Collect PowerShell ScriptAnalyzer reports to an Excel Workbook
+.DESCRIPTION
+	Leverages the PSScriptAnalyzer and ImportExcel modules to generate and Excel workbook with analysis.
+.PARAMETER xlFile
+	Specifies a full path for the resulting report file. Must include the extension with value of XLSX.
+.PARAMETER repoFolder
+	Specifies path to folder containing the scripts to be reported on.
+.PARAMETER errOnly
+	Specifies the report only include entries with the Severity of Error.
+.EXAMPLE
+	PS>_ ./get-pssareport.ps1
+	Example of how to use this script to create a full PSSA report
+.EXAMPLE
+	PS>_ ./get-pssareport.ps1 -errOnly
+	Another example of how to use this script to create a PSSA report of Errors only.
+.NOTES
+	===========================================================================
+	Created with:	Visual Studio Code
+	Created on:		11.05.2019
+	Created by:		John J. Kavanagh
+	Organization:	KavanaghTech
+	Filename:		get-pssareport.ps1
+	===========================================================================
+	11.05.2019 JJK:	DONE: Formalize to Include frompipeline as well as pattern checking for xlsx
+#>
 [CmdletBinding()]
 Param (
-	[System.IO.FileInfo]$xlfile = "c:\temp\slackreport.xlsx",
+	[parameter(Mandatory = $False, ValueFromPipeline = $True,
+		HelpMessage = "Excel file information")]
+	[ValidateScript({
+			If ($_.Extension.ToUpper().EndsWith("XLSX")) {
+				"Excel file"
+			}
+			Else {
+				throw "The file extension must be XLSX. "
+			}
+		})]
+	[System.IO.FileInfo]$xlFile = "c:\temp\slackreport.xlsx",
 	[System.IO.DirectoryInfo]$repoFolder = "C:\users\johnk\Documents\github\slackgroup",
 	[parameter(Mandatory = $False, ValueFromPipeline = $True,
 		HelpMessage = "Specify to only generate report of errors")]
@@ -8,9 +46,9 @@ Param (
 )
 Begin {
 	# Clean up report file
-	If (Test-Path $xlfile) {
+	If (Test-Path $xlFile) {
 		"Deleting existing ScriptAnalyzer Report"
-		Remove-Item $xlfile -Force
+		Remove-Item $xlFile -Force
 	}
 }
 Process {
@@ -28,10 +66,10 @@ Process {
 	}
 	# Run PSSA reporting only errors
 	If ($errOnly) {
-		Invoke-ScriptAnalyzer -Severity Error -Path $repoFolder | Export-Excel $xlfile @p
+		Invoke-ScriptAnalyzer -Severity Error -Path $repoFolder | Export-Excel $xlFile @p
 	}
 	Else {
-		Invoke-ScriptAnalyzer -Path $repoFolder | Export-Excel $xlfile @p
+		Invoke-ScriptAnalyzer -Path $repoFolder | Export-Excel $xlFile @p
 	}
 }
 End {

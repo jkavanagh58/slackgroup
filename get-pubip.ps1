65819e8ae8e59e0e77c88544809238d87d784aa1
Kavanagh, John J <jkavpowershell@outlook.com>
2020-09-19 10:58:10 -0400

    ✨ Util: Get Public IP Adding switch parameters to specify IP Address type

--- get-pubip.ps1
+++ get-pubip.ps1
@@ -1,1 +1,15 @@
-(Invoke-WebRequest -Uri "https://ipv4.icanhazip.com/").content
+[CmdletBinding()]
+Param (
+	[parameter(Mandatory = $False, ValueFromPipeline = $True,
+		HelpMessage = 'IP4 Output')]
+	[Switch]$IPV4,
+	[parameter(Mandatory = $False, ValueFromPipeline = $True,
+		HelpMessage = 'IP6 Output')]
+	[Switch]$IPV6
+)
+If ($IPV4) {
+	(Invoke-WebRequest -Uri 'https://ipv4.icanhazip.com/').content
+}
+If ($IPV6) {
+	(Invoke-WebRequest -Uri 'https://ipv6.icanhazip.com/').content
+}
